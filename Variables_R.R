295a58a9a200232ccdbb62dd1546ec02ef04be2b
Aishwarya Mate <118876963+aishwaryamate@users.noreply.github.com>
2023-09-28 09:34:57 +0530

    Add files via upload

--- Variables_R.R
+++ Variables_R.R
@@ -9,12 +9,15 @@
 #In R we can use both (= and <-) as a assignment operator, but
 #mostly <- operator is preffered over =.
 
-a <- 10+5
-b5 = 50
-a
-A <- 'Python'
+1ten = 10
+ten1 <- 5
+coursename <- 'DA'
+course name <- 'DA'
+course_name = 'DS'
+a! = 53
 
-python <- 'Python'
+rm(ten1)
+rm(coursename)
 
 #Data types in R:
 #1.Integer
@@ -22,96 +25,115 @@ python <- 'Python'
 #3.String/character
 #4.Logical/Boolean
 
-a = 100
-b = 10.5
+ten = 10
+class(ten)
+
+f = 10.2
+class(f)
+
+#L letter will define integer value.
+a <- 50L
 class(a)
-class(b)
 
-x <- 100L
+city <- 'PUNE'
+class(city)
+
+pin_code = '54614'
+class(pin_code)
+
+
+boolean <- TRUE
+class(boolean)
+x <- F
 class(x)
-
-obj <- 'We are learning R'
-class(obj)
-x <- '500'
-class(x)
-
-bool <- TRUE
-class(bool)
+a <- 'TRUE'
+class(a)
 
 
-x <- 100
-y <- 50
-z <- x+y
-z
 
-name9<- 'ABC'
-marks <- 85,98,75,85,65
+age <- 20,21,25,26,23
+
 
 #Data structures in R
 #Vector and Data Frame, Lists
 
 #Homogeneous Data Structure(Vector)
 
-marks <- c(85,98,75,85,65)
-names <- c('Sayali','Abhishek','A','B','C')
+age <- c(20,21,22,25,26)
 
-info <- c('Sayali',85,5.2,TRUE)
+info <- c('Sakshi',25,145.32,TRUE)
 info
 
-a <- c(85,5.3,T,'sayali')
-a
-info
+info1 <- c(25,145.32,TRUE)
+info1
+
 
 #Accessing elements from a vector
+info <- c('Sakshi',25,145.32,TRUE)
+info
 info[2]
 info[4]
-info[1:2]
-info[2:4]
+info[1:3]
+info[1,2,3]
+info[2:3]
+info[1,4]
 info[c(1,4)]
-names
-match('A',names)
-match(TRUE, info)
+info[c(1,3)]
+info[c(1,3,4)]
 
-
+match(25,info) #Used to find out the index number of any value.
+match(TRUE,info)
+info <- c('Sakshi',25,145.32,TRUE)
 #Assigning or replacing values in a vector
 
-names
+info[2] <- 35
+info
 
-names[2] <- 'TRUE'
-names
-names[1:2] <- c('Z','FALSE')
-names
+info[c(1,4)]
+info[c(1,4)] <- c('Tarun',"FALSE")
+info
 
 #List Creation
-lst <- list(10L,52.2,TRUE,'Python')
+lst_ds = list('Sakshi',20,165.52,T)
 lst
+
 #Accessing and replacing elements from list
-lst[3]
+
 lst[2:4]
-lst[3] <- FALSE
+lst[3]
+lst[c(1,4)]
+lst[3] <- 'Pune'
 lst
+
 
 #Data Frame Creation
-ID <- c(101,102,103,104,105)
-Names <- c('A','B','C','D','E')
-Age <- c(20,23,21,25,26)
-Dept <- c('HR','Acc','Marketing','Scientist','Analyst')
 
-df <- data.frame(ID,Names,Age,Dept)
+ID <- c(100,101,102,103,104)
+Name <- c('A','B','C','D','E')
+Age <- c(20,23,25,30,35)
+Dept <- c('HR','Accounts','DA','DS','Marketing')
+City <- c('Pune','Mumbai','HYD','Delhi','Chennai')
+
+df <- data.frame(ID,Name,Age,Dept,City)
+df
 View(df)
+
+
 #Accessing elements from a data frame
+
+df[1,5]
+df[3,4]
 df[5,4]
-df[1,2,3]
-df[1,1:3]
-df[1,c(2,4)]
-df[c(1,5),1]
+df[1:2,4:5]
+df[4:5,2:3]
+df[1,c(1,4)]
+df[c(2,5),5]
+df[c(2,5),c(1,5)]
 
 #Replacing values from a data frame.
-
-df[2,4] <- 'Accounts'
-
-df[4:5,4] <- c('Data Scientist','Data Analyst')
-
+df[3,4]
+df[3,4] <- 'Data Analyst'
+df[4,4] <- 'Data Scientist'
 
 
-
+df[2,c(1,5)] <- c(201,'Bengaluru')
